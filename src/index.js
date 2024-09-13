2e3b0c900c5400584e3eeca0b7611ed8cd2d18ce
Olga <olgaalaquas@gmail.com>
2024-09-13 12:12:15 +0200

    created the consults DB

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ server.get('/projects', async (req, res) => {
   //try para errores 
   try {
     const connection = await getDBConnection();
-    const sqlQuery = "SELECT * FROM freedb_ProyectosMolones.autor AS autor, freedb_ProyectosMolones.proyectos AS projects WHERE autor.id = projects.fk_autor_id LIMIT 4;";
-    const [projectsResult] = await connection.query(sqlQuery);
+    const sqlQuery = "SELECT * FROM freedb_ProyectosMolones.autor AS autor, freedb_ProyectosMolones.proyectos AS projects WHERE autor.id = projects.fk_autor_id LIMIT 4 OFFSET 1";
+    const [projectsResult] = await connection.query(sqlQuery); 4;
     await connection.end();
 
     res.json({
