185b2bd3a6386ec76185b54e07543cb70865b10b
YesseniaColocho <Colocho.yessenia@gmail.com>
2024-09-15 12:54:05 +0200

    Fix merge error

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ const server = express();
 server.use(cors());
 
 server.use(express.json());
-server.set('view engine', 'ejs');
-
+server.set("view engine", "ejs");
 
 async function getDBConnection() {
   const connection = await mysql.createConnection({
@@ -28,17 +27,17 @@ async function getDBConnection() {
 
 // establecer el puerto de conexión
 const port = 5002;
-const port = 5002;
 
 server.listen(port, () => {
   console.log("Server is running on port " + port);
 });
 
-server.get('/projects', async (req, res) => {
-  //try para errores 
+server.get("/projects", async (req, res) => {
+  //try para errores
   try {
     const connection = await getDBConnection();
-    const sqlQuery = "SELECT * FROM freedb_ProyectosMolones.autor AS autor, freedb_ProyectosMolones.proyectos AS projects WHERE autor.id = projects.fk_autor_id LIMIT 4 OFFSET 1";
+    const sqlQuery =
+      "SELECT * FROM freedb_ProyectosMolones.autor AS autor, freedb_ProyectosMolones.proyectos AS projects WHERE autor.id = projects.fk_autor_id LIMIT 4 OFFSET 1";
     const [projectsResult] = await connection.query(sqlQuery);
     await connection.end();
 
@@ -56,12 +55,13 @@ server.get('/projects', async (req, res) => {
   }
 });
 
-// endpoint para añadir un nuevo proyecto 
-server.post("/projects", async(req, res) => {
+// endpoint para añadir un nuevo proyecto
+server.post("/projects", async (req, res) => {
   try {
     const connection = await getDBConnection();
     const params = req.body;
-    const autorQuerySQL = "INSERT INTO autor (name, job, photo) VALUES(?, ?, ?)";
+    const autorQuerySQL =
+      "INSERT INTO autor (name, job, photo) VALUES(?, ?, ?)";
     const [autorResult] = await connection.query(autorQuerySQL, [
       params.name,
       params.job,
@@ -69,7 +69,8 @@ server.post("/projects", async(req, res) => {
     ]);
 
     const newAutor = autorResult.insertId;
-    const projectQuerySQL =  "INSERT INTO proyectos (proyectName, description, slogan, tecnologies, image, github, demo, fk_autor_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    const projectQuerySQL =
+      "INSERT INTO proyectos (proyectName, description, slogan, tecnologies, image, github, demo, fk_autor_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     const [projectResult] = await connection.query(projectQuerySQL, [
       params.proyectName,
       params.description,
@@ -78,13 +79,13 @@ server.post("/projects", async(req, res) => {
       params.image,
       params.github,
       params.demo,
-      newAutor
+      newAutor,
     ]);
 
     connection.end();
     res.json({
       status: "success",
-      result : projectResult,
+      result: projectResult,
       cardUrl: `//localhost:5002/detail/${projectResult.insertId}`,
     });
   } catch (e) {
@@ -97,17 +98,17 @@ server.post("/projects", async(req, res) => {
 });
 
 //para renderizar la tarjeta con motores de plantillas.
-server.get('/detail/:id', async (req, res) => {
+server.get("/detail/:id", async (req, res) => {
   const connection = await getDBConnection();
   const id = req.params.id;
-  const sqlQuery = "SELECT * FROM proyectos, autor where proyectos.id = ? and proyectos.fk_autor_id = autor.id;";
+  const sqlQuery =
+    "SELECT * FROM proyectos, autor where proyectos.id = ? and proyectos.fk_autor_id = autor.id;";
   const [result] = await connection.query(sqlQuery, [id]);
   connection.end();
-  res.render("detail", {result: result[0]});
+  res.render("detail", { result: result[0] });
 });
 
 //agregar el servidor estatico
- 
+
 const staticServer = "./src/public-react";
 server.use(express.static(staticServer));
-
